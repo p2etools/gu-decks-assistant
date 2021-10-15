1dc704ea01c6c89784f6dd4da1efcef72187e400
Chau Giang <giangc@acm.org>
2021-10-15 09:06:50 +0700

    fix: Fix bug auto update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
+const { app, BrowserWindow, ipcMain, nativeTheme, autoUpdater } = require("electron");
 const {
   Socket,
   Transport,
